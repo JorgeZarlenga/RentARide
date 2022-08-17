064d05f8a6a93cddc643b668d5022fdc363042df
Jorge Zarlenga Regiani <jorge.zarlenga@outlook.com>
2022-08-16 21:07:26 -0300

    Adjusted spacing of settings and added icons

--- screens/Profile/ProfileScreen.js
+++ screens/Profile/ProfileScreen.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 
 import styles from './profile-screen.styles';
@@ -10,18 +10,23 @@ const ProfileScreen = ({navigation}) => {
 
     const settingsOptions = [
         {
+            icon: require('./../../assets/icons/compass.png'),
             settingTitle: 'Notificações'
         },
         {
+            icon: require('./../../assets/icons/compass.png'),
             settingTitle: 'Meus cartões'
         },
         {
+            icon: require('./../../assets/icons/compass.png'),
             settingTitle: 'Meus dados'
         },
         {
+            icon: require('./../../assets/icons/compass.png'),
             settingTitle: 'Termos e condições'
         },
         {
+            icon: require('./../../assets/icons/compass.png'),
             settingTitle: 'Fale conosco'
         }
     ];
@@ -38,19 +43,30 @@ const ProfileScreen = ({navigation}) => {
                     <Text>Imagem de perfil</Text>
                 </View>
             </View>
+                <ScrollView>
             <View style={styles.settingsContainer}>
                 {/* ScrollView para garantir a visualização de toda a lista mesmo em telas pequenas */}
-                <ScrollView>
-                    {settingsOptions.map(( {settingTitle}, index ) =>
+                    {settingsOptions.map(( {icon, settingTitle}, index ) =>
                     <TouchableOpacity key={settingTitle}>
-                        <View>
-                            <Text>{settingTitle}</Text>
+                        <View style={styles.settingContainer}>
+                            <View style={[styles.settingImageContainer, styles.elementsCentered]}>
+                            <Image
+                                source={icon}
+                                style={{width: 26, height: 26, fill: '#8F9BB3'}}
+                                />
+                            </View>
+                            <View style={styles.settingTextContainer}>
+                                <Text>{settingTitle}</Text>
+                            </View>
+                            <View style={[styles.settingArrowContainer, styles.elementsCentered]}>
+                                <Text>></Text>
+                            </View>
                         </View>
                         {/* View usada como divider */}
                         <View style={styles.settingDivider}/>
                     </TouchableOpacity>)}
-                </ScrollView>
             </View>
+                </ScrollView>
         </View>
     );
 };
--- screens/Profile/profile-screen.styles.js
+++ screens/Profile/profile-screen.styles.js
@@ -15,6 +15,28 @@ const styles = StyleSheet.create({
     width: '65%',
     backgroundColor: 'orange'
   },
+  settingContainer: {
+    height: 80,
+    display: 'flex',
+    flexDirection: 'row',
+    flex: 1
+  },
+  settingImageContainer: {
+    width: '15%',
+  },
+  settingTextContainer: {
+    justifyContent: 'center',
+    paddingLeft: 20,
+    width: '70%',
+  },
+  settingArrowContainer: {
+    width: '15%',
+    flex: 1
+  },
+  elementsCentered: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   settingDivider: {
     borderBottomColor: '#000',
     borderBottomWidth: StyleSheet.hairlineWidth,
